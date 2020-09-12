1514d3a3d63bbf746b9a31c33af27beb43b4be0b
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-09-12 11:16:46 -0300

    Modificando admin.

--- cursos/admin.py
+++ cursos/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Curso, Avaliacao
 
 
-admin.site.register(Curso)
-admin.site.register(Avaliacao)
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo')
+
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('curso', 'nome', 'email', 'avaliacao', 'criacao', 'atualizacao', 'ativo')
