b0a83f9d0bc2688f2707ecd54a52d72c8a820a74
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-09-14 12:23:48 -0300

    Criando CursoApiView.

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoAPIView(APIView):
+    """
+       Curso de Api
+    """
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)#Muitos cursos
+        return Response(serializer.data)
